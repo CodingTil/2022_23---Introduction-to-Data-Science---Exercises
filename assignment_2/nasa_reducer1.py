22a2e0066ef7b5bf711add7fcda65b8220a1e360
CodingTil <36734749+CodingTil@users.noreply.github.com>
2023-01-19 12:39:58 +0100

    Q7 fix

--- assignment_2/nasa_reducer1.py
+++ assignment_2/nasa_reducer1.py
@@ -5,58 +5,30 @@ current_execution_id = None
 current_activity = None
 current_lifecycle_transition = None
 current_timestamp = None
-total_time = 0
-start_count=0
-complete_count=0
 
 for line in sys.stdin:
     line = line.strip()
     execution_id, lifecycle_transition, activity, timestamp = line.split("\t")
     if current_execution_id is not None:
+        # execution id must match (there are always 2)
         if current_execution_id != execution_id:
-            if start_count > 1 or complete_count > 1:
-                raise Exception("Unexpected number of start or complete")
-            if start_count == 1 and complete_count == 1:
-                print("{}\t{}".format(current_activity, total_time))
-            current_execution_id = None
-            current_activity = None
-            current_lifecycle_transition = None
-            current_timestamp = None
-            total_time = 0
-            start_count=0
-            complete_count=0
-        else:
-            if current_lifecycle_transition == "start":
-                if lifecycle_transition == "complete":
-                    assert total_time == 0
-                    total_time = float(timestamp) - current_timestamp
-                    complete_count+=1
-                else:
-                    raise Exception("Unexpected lifecycle transition")
-            elif current_lifecycle_transition == "complete":
-                if lifecycle_transition == "start":
-                    assert total_time == 0
-                    total_time = current_timestamp - float(timestamp)
-                    start_count+=1
-                else:
-                    raise Exception("Unexpected lifecycle transition")
-            else:
-                raise Exception("Unexpected lifecycle transition")
+            raise Exception("Unexpected execution_id")
+
+        # there must be exactly 1 start and 1 complete
+        if not ((current_lifecycle_transition == "start" and lifecycle_transition == "complete")
+            or (current_lifecycle_transition == "complete" and lifecycle_transition == "start")):
+            raise Exception("Unexpected lifecycle transition")
+
+        if not (current_activity == activity):
+            raise Exception("Unexpected activity")
+
+        print("{}\t{}".format(current_activity, abs(current_timestamp - float(timestamp))))
+        current_execution_id = None
+        current_activity = None
+        current_lifecycle_transition = None
+        current_timestamp = None
     else:
         current_execution_id = execution_id
         current_activity = activity
         current_lifecycle_transition = lifecycle_transition
         current_timestamp = float(timestamp)
-        if lifecycle_transition == "start":
-            start_count+=1
-        elif lifecycle_transition == "complete":
-            complete_count+=1
-        else:
-            raise Exception("Unexpected lifecycle transition")
-
-# Print the last execution_id
-if start_count > 1 or complete_count > 1:
-    raise Exception("Unexpected number of start or complete")
-if start_count == 1 and complete_count == 1:
-    assert current_activity is not None
-    print("{}\t{}".format(current_activity, total_time))
